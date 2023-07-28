ed3cc808c53f5a17cb3a6cda3fa5094622742c6e
tumble <tumblegamer@gmail.com>
2023-07-28 14:25:15 +0100

    only deploy to cloudflare on master  fix emit event function

--- src/userscripts/index.js
+++ src/userscripts/index.js
@@ -1,25 +1,3 @@
-import emitEvent from '../utils/emitEvent';
-
-// const userscripts = [
-// 	'./GM_addStyle',
-// 	'./check-userscript',
-// 	'./sky-remote.user',
-// 	//'./beehive-bedlam.user',
-// 	//'./template.user',
-// 	//'./sky-remote-mobile.user',
-// 	'./gamepad-support.user'
-// ].map(file => import(file));
-
-import SkyRemote from './SkyRemote.user';
-
-import './gamepadSupport.user';
-
-
 export async function initUserscripts(window) {
 	console.error("No dont use this, we use propper modules now");
 }
-
-window.SkyRemote = SkyRemote;
-
-
-export default { SkyRemote };
